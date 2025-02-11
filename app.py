174acb3d83578258c9446058828b2c1146f60bb5
Jorge Pereira <JorgePe@users.noreply.github.com>
2025-02-11 11:19:29 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request , redirect
+import paho.mqtt.publish as mqttpub
+
+app = Flask(__name__)
+
+mqtt_broker = 'test.mosquitto.org'
+mqtt_topic = '/plug/teste'
+
+@app.route('/publish')
+def publish():
+    # extracts a msg from the called URL
+    msg = request.args.get('msg')
+    # and forwards it to a MQTT broker
+    mqttpub.single(mqtt_topic, msg, hostname=mqtt_broker)
+    # then redirects visitor to a page that informs what was done
+    return render_template('aviso.html', msg = 'Comando enviado para ' + msg)
+
+@app.route('/')
+@app.route('/home')
+def index():
+    # renders a short web page
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
